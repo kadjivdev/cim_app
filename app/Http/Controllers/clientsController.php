484fa0f812d3a2c9ebe1bd7a10668b37929f10cf
kadjivdev <kadjivdev@gmail.com>
2025-12-03 09:08:03 +0100

    revu du clientsController

--- app/Http/Controllers/clientsController.php
+++ app/Http/Controllers/clientsController.php
@@ -36,7 +36,6 @@ class clientsController extends Controller
             });
         }
 
-
         // UN AGENT NE VERA QUE LES CLIENTS SE TROUVANT DANS LA ZONE DE SON REPRESENTANT
         $user = Auth::user();
 
@@ -108,7 +107,11 @@ class clientsController extends Controller
         // UN AGENT NE VERA QUE LES CLIENTS SE TROUVANT DANS LA ZONE DE SON REPRESENTANT
         $user = Auth::user();
 
-        if (!(Auth::user()->roles()->where('libelle', 'ADMINISTRATEUR')->exists() || Auth::user()->roles()->where('libelle', ['CONTROLEUR'])->exists()) && Auth::user()->roles()->where('libelle', ['VENDEUR'])->exists()) {
+        if (
+            !(Auth::user()->roles()->where('libelle', 'ADMINISTRATEUR')->exists() ||
+                Auth::user()->roles()->where('libelle', ['CONTROLEUR'])->exists()) &&
+            Auth::user()->roles()->where('libelle', ['VENDEUR'])->exists()
+        ) {
             $clients = $clients->where("zone_id", $user->zone_id);
         }
 
